d5b6090534072a3e1048774af5b78fa55751cc34
Hieu Tran <tthieu0901@gmail.com>
2025-07-30 12:48:34 +0700

    feat: Add send PONG for each PING request and Client for testing local

---
+++ src/main/java/Client.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        try (Socket socket = new Socket()) {
+            socket.connect(new InetSocketAddress("localhost", 6379));
+            System.out.println("Connected to server");
+            OutputStream outputStream = socket.getOutputStream();
+            outputStream.write("PING\nPING".getBytes());
+
+            InputStream inputStream = socket.getInputStream();
+            byte[] buffer = new byte[1024];
+            int bytesRead = inputStream.read(buffer);
+            if (bytesRead == -1) {
+                throw new IOException("No data received");
+            }
+            System.out.println(new String(buffer, 0, bytesRead));
+            System.out.println("Disconnected from server");
+            inputStream.close();
+            outputStream.close();
+            System.out.println("Closing socket");
+        } catch (IOException e) {
+            System.out.println("Could not connect to server");
+            throw e;
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,40 +1,65 @@
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        int port = 6379;
+        try {
+            serverSocket = new ServerSocket(port);
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
+            // Wait for connection from client.
+            clientSocket = serverSocket.accept();
 
-    //  Uncomment this block to pass the first stage
-    ServerSocket serverSocket = null;
-    Socket clientSocket = null;
-    int port = 6379;
-    try {
-      serverSocket = new ServerSocket(port);
-      // Since the tester restarts your program quite often, setting SO_REUSEADDR
-      // ensures that we don't run into 'Address already in use' errors
-      serverSocket.setReuseAddress(true);
-      // Wait for connection from client.
-      clientSocket = serverSocket.accept();
-      sendMessage(clientSocket,  "+PONG\r\n");
-    } catch (IOException e) {
-      System.out.println("IOException: " + e.getMessage());
-    } finally {
-      try {
-        if (clientSocket != null) {
-          clientSocket.close();
+            handleClientSocket(clientSocket);
+
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        } finally {
+            try {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
         }
-      } catch (IOException e) {
-        System.out.println("IOException: " + e.getMessage());
-      }
     }
-  }
 
-  private static void sendMessage(Socket socket, String message) throws IOException {
-    OutputStream outputStream = socket.getOutputStream();
-    outputStream.write(message.getBytes());
-  }
+    private static void handleClientSocket(Socket clientSocket) throws IOException {
+        List<String> messages = readMessage(clientSocket);
+
+        for (String message : messages) {
+            System.out.println(message);
+            sendMessage(clientSocket, "+PONG\r\n");
+        }
+    }
+
+    private static List<String> readMessage(Socket socket) {
+        InputStream inputStream = null;
+        try {
+            inputStream = socket.getInputStream();
+            byte[] buffer = new byte[1024];
+            int bytesRead = inputStream.read(buffer);
+            String message = new String(buffer, 0, bytesRead);
+            System.out.println("Received message: " + message);
+            return Arrays.stream(message.split("\n")).toList();
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+        return List.of();
+    }
+
+    private static void sendMessage(Socket socket, String message) throws IOException {
+        OutputStream outputStream = socket.getOutputStream();
+        outputStream.write(message.getBytes());
+    }
 }
