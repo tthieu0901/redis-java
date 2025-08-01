13a1bdfab55526e2aa5325ea806e69fe07b4a188
Hieu Tran <tthieu0901@gmail.com>
2025-08-01 15:04:48 +0700

    feat: Empty commit to trigger CI

--- src/main/java/utils/RedisProcessor.java
+++ src/main/java/utils/RedisProcessor.java
@@ -2,8 +2,4 @@ package utils;
 
 public class RedisProcessor {
     public static final String CRLF = "\r\n";
-
-    public static String sanitizeData(String data) {
-        return data.replaceAll("\r\n", "");
-    }
 }
